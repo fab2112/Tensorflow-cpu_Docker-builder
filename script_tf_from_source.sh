57bd73565c12c3917403fd928a44c4ce27333bbb
Fabrício Siqueira <35580812+fab2112@users.noreply.github.com>
2025-02-16 20:23:20 -0300

    Update due to changes in build from source mode

--- script_tf_from_source.sh
+++ script_tf_from_source.sh
@@ -2,10 +2,11 @@
 
 echo "\n\nTENSORFLOW-FROM-SOURCE-SCRIPT\n"
 
-git clone https://github.com/tensorflow/tensorflow.git
+git clone --depth 1 --branch v2.18.0 https://github.com/tensorflow/tensorflow.git
 cd tensorflow/
 ./configure 
-bazel build --config=opt //tensorflow/tools/pip_package:build_pip_package
-./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tf_wheel/
+bazel build --config=opt //tensorflow/tools/pip_package:wheel --repo_env=WHEEL_NAME=tensorflow_cpu  
+
+cp bazel-bin/tensorflow/tools/pip_package/wheel_house/* /tf_wheel/  
 
 exit 0
